0ace970631218ea14267c1de409eae99e83021fd
Lorenz <lorenz.kort@gmail.com>
2020-10-04 12:55:47 +0200

    Added keywords CDD KYC

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from monsterboard import check_monsterboard
 from careerguide import check_careerguide
 
+# datakwaliteit
 kws = ['Data Stewardship', 'Data Steward', 'Datakwaliteit']
 for kw in kws:
-    check_monsterboard(kw)
-    check_careerguide(kw)
+    check_monsterboard(kw, chat_id='-459671235')
+    check_careerguide(kw, chat_id='-459671235')
+
+# customer due diligence
+kws = ['CDD' , 'KYC', 'Transactiemonitoring']
+for kw in kws:
+    check_monsterboard(kw, '-487901102')
+    check_careerguide(kw, '-487901102')
